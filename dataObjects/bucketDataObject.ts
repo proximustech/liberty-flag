39848a16c1b90226625a0998886d26d26fa4fc6f
Proximus <user@proximustech.net>
2024-09-03 00:26:04 -0500

    .

--- dataObjects/bucketDataObject.ts
+++ dataObjects/bucketDataObject.ts
@@ -1,3 +1,5 @@
+import { validateFunction } from "../../../values/dataObjectValidateFunction";
+
 export class BucketDataObject {
     _id:any = ""
     uuid:string = ""
@@ -14,24 +16,5 @@ export const BucketDataObjectValidator:any = {
         },
     },
 
-    validateFunction : (data:any,validateData:any) => {
-        let fieldRegexp = ""
-        let fieldValue = ""
-        let result = []
-        let regexpValidator = undefined
-
-        for (const [fieldName, fieldData] of Object.entries(validateData)) {
-            fieldRegexp = validateData[fieldName]["regexp"]
-            fieldValue = data[fieldName]
-
-            regexpValidator = new RegExp(fieldRegexp);
-            if (!regexpValidator.test(fieldValue)) {
-                result.push({
-                    field:fieldName,
-                    message:validateData[fieldName]["message"]
-                })
-            }
-        }
-        return result
-    }
+    validateFunction : validateFunction
 }

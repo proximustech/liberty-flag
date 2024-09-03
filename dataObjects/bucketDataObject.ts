42e7731069624e9f28d6f2e8dd55f1659197b958
Proximus <user@proximustech.net>
2024-09-03 00:09:52 -0500

    BucketDataObjectValidator Test

--- dataObjects/bucketDataObject.ts
+++ dataObjects/bucketDataObject.ts
@@ -4,3 +4,34 @@ export class BucketDataObject {
     name:string = ""
     subBuckets:Array<BucketDataObject> = []
 }
+
+export const BucketDataObjectValidator:any = {
+
+    validateData : {
+        name : {
+            regexp:".{5,30}",
+            message:"Name must be in the range of 5 and 30 characters."
+        },
+    },
+
+    validateFunction : (data:any,validateData:any) => {
+        let fieldRegexp = ""
+        let fieldValue = ""
+        let result = []
+        let regexpValidator = undefined
+
+        for (const [fieldName, fieldData] of Object.entries(validateData)) {
+            fieldRegexp = validateData[fieldName]["regexp"]
+            fieldValue = data[fieldName]
+
+            regexpValidator = new RegExp(fieldRegexp);
+            if (!regexpValidator.test(fieldValue)) {
+                result.push({
+                    field:fieldName,
+                    message:validateData[fieldName]["message"]
+                })
+            }
+        }
+        return result
+    }
+}
--- routes/_liberty_flag_routes.ts
+++ routes/_liberty_flag_routes.ts
@@ -1,16 +1,29 @@
 import Router from "koa-router"
 import { BucketService } from "../services/bucketService";
+import { BucketDataObjectValidator } from "../dataObjects/BucketDataObject";
 import koaBody from 'koa-body';
 
 let getRouter = (viewVars: any) => {
     const prefix = 'liberty_flag'
     const router = new Router({prefix: '/'+ prefix});
+    viewVars.prefix = prefix
 
     router.get('/buckets', async (ctx) => {
         try {
             const bucketService = new BucketService()
             viewVars.buckets = await bucketService.getAll()
             return ctx.render('plugins/_'+prefix+'/views/buckets', viewVars);
+        } catch (error) {
+            console.error(error)
+        }
+    })
+
+    router.get('/bucket_create_form', async (ctx) => {
+        try {
+            viewVars.validateData = JSON.stringify(BucketDataObjectValidator.validateData)
+            viewVars.validateFunction = "validateFunction=" + BucketDataObjectValidator.validateFunction
+
+            return ctx.render('plugins/_'+prefix+'/views/bucket_create_form', viewVars);
         } catch (error) {
             console.error(error)
         }

12223cfac6836955a62d676f11bbcedf3a8fecdd
Proximus <user@proximustech.net>
2024-09-05 01:06:13 -0500

    Bucket Ok showing validated field control

--- dataObjects/bucketDataObject.ts
+++ dataObjects/bucketDataObject.ts
@@ -19,9 +19,14 @@ export const BucketDataObjectValidator:any = {
 
     validateSchema : {
         name : {
-            regexp:".{5,30}",
+            regexp:"^.{5,30}$",
             message:"Name must be in the range of 5 and 30 characters."
         },
+        description : {
+            regexp:"^.{10,30}$",
+            message:"Name must be in the range of 10 and 30 characters."
+        },
+
     },
 
     validateFunction : DataObjectValidateFunction
@@ -32,14 +37,19 @@ export const BucketDataObjectSpecs:any = {
     metadata : {
         name : {
             label:"Bucket Name",
-            regexp: BucketDataObjectValidator.validateSchema.name.regexp,
+            validationRegexp: BucketDataObjectValidator.validateSchema.name.regexp,
+            validationMessage: BucketDataObjectValidator.validateSchema.name.message,
             inputType:"text"
         },
-        context : {
-            label:"Context Name",
-            regexp: BucketDataObjectValidator.validateSchema.name.regexp,
-            inputType:"text"
+        /*
+        description : {
+            label:"Bucket Description",
+            validationRegexp: BucketDataObjectValidator.validateSchema.description.regexp,
+            validationMessage: BucketDataObjectValidator.validateSchema.description.message,
+            inputType:"text_area"
         },
+        */
+
     },
     htmlDataObjectRender:HtmlDataObjectRender,
     htmlDataObjectFieldRender:HtmlDataObjectFieldRender

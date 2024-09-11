ccfcd17b32bf3f460b8cb75987494b54f848ea81
Proximus <user@proximustech.net>
2024-09-11 00:21:52 -0500

    Bucket. Implement requried property in validation

--- dataObjects/bucketDataObject.ts
+++ dataObjects/bucketDataObject.ts
@@ -16,11 +16,15 @@ export const BucketDataObjectValidator:any = {
     validateSchema : {
         name : {
             regexp:"^.{5,30}$",
-            message:"Bucket name must be in the range of 5 and 30 characters."
+            message:"Bucket name must be in the range of 5 and 30 characters.",
+            required:true,
+            requiredMessage : "Bucket name is required."
         },
         description : {
             regexp:"^(.|\\n){30,200}$$",
-            message:"Bucket description must be in the range of 30 and 200 characters."
+            message:"Bucket description must be in the range of 30 and 200 characters.",
+            required:false,
+            requiredMessage : ""
         },
 
     },
@@ -33,15 +37,19 @@ export const BucketDataObjectSpecs:any = {
     metadata : {
         name : {
             label:"Bucket Name",
+            validationRequired: BucketDataObjectValidator.validateSchema.name.required,
             validationRegexp: BucketDataObjectValidator.validateSchema.name.regexp,
             validationMessage: BucketDataObjectValidator.validateSchema.name.message,
+            validationRequiredMessage: BucketDataObjectValidator.validateSchema.name.requiredMessage,
             inputType:"text"
         },
         
         description : {
             label:"Bucket Description",
+            validationRequired: BucketDataObjectValidator.validateSchema.description.required,
             validationRegexp: BucketDataObjectValidator.validateSchema.description.regexp,
             validationMessage: BucketDataObjectValidator.validateSchema.description.message,
+            validationRequiredMessage: BucketDataObjectValidator.validateSchema.description.requiredMessage,
             inputType:"text_area"
         },
 
@@ -60,7 +68,9 @@ export const BucketContextDataObjectValidator:any = {
     validateSchema : {
         name : {
             regexp:"^.{1,20}$",
-            message:"Context name must be in the range of 1 and 20 characters."
+            message:"Context name must be in the range of 1 and 20 characters.",
+            required:true,
+            requiredMessage:"Context name is required.",
         },
 
     },
@@ -73,8 +83,10 @@ export const BucketContextDataObjectSpecs:any = {
     metadata : {
         name : {
             label:"Context Name",
+            validationRequired: BucketContextDataObjectValidator.validateSchema.name.required,
             validationRegexp: BucketContextDataObjectValidator.validateSchema.name.regexp,
             validationMessage: BucketContextDataObjectValidator.validateSchema.name.message,
+            validationRequiredMessage: BucketContextDataObjectValidator.validateSchema.name.requiredMessage,
             inputType:"text"
         },
 

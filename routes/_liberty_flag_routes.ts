fecb03f321c4d7cd550f16691714907420f003d6
Julián Arias <mail@proximustech.net>
2024-11-04 02:54:18 -0500

    Routes. Relocate viewVars prefix assignment

--- routes/_liberty_flag_routes.ts
+++ routes/_liberty_flag_routes.ts
@@ -3,7 +3,6 @@ import Router from "koa-router"
 let getRouter = (viewVars: any) => {
     const prefix = 'liberty_flag'
     let router = new Router({prefix: '/'+ prefix});
-    viewVars.prefix = prefix
 
     router = require("./bucket_routes.ts")(router,viewVars,prefix)
     router = require("./tag_routes.ts")(router,viewVars,prefix)

e987a5ad579205e1e9adef94e042f1f5b0852eb3
Julian Arias <soporte@proximustech.net>
2025-05-15 01:43:49 -0500

    Dynamic Views. Export as Default

--- services/module_menu.ts
+++ services/module_menu.ts
@@ -1,7 +1,7 @@
 import {IDynamicView} from "../../../interfaces/dynamic_view_interface"
 import { UserHasPermissionOnElement } from "../../users_control/services/UserPermissionsService";
 
-export class ModuleMenu implements IDynamicView {
+export default class ModuleMenu implements IDynamicView {
     async getPluginData(ctx:any,viewVars:any){
         viewVars.UserHasPermissionOnElement = UserHasPermissionOnElement
         return await ctx.render('plugins/_liberty_flag/views/module_menu', viewVars);
--- services/permissions.ts
+++ services/permissions.ts
@@ -1,7 +1,7 @@
 import {IDynamicView} from "../../../interfaces/dynamic_view_interface"
 import { LibertyFlag_permissionsSchema } from "../values/PermissionsSchema";
 
-export class Permissions implements IDynamicView {
+export default class Permissions implements IDynamicView {
     async getPluginData(ctx:any,viewVars:any){
         viewVars.libertyFlag_permissionsSchema = LibertyFlag_permissionsSchema
         return await ctx.render('plugins/_liberty_flag/views/permissions', viewVars);
